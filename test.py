e42d06f0cbcabac6bc27651f452bc1e1a372ec46
Bruno <brunofaomoura@gmail.com>
2026-07-22 19:23:41 -0300

    primeiro commit do projeto automacao-organizador-arquivos

---
+++ test.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+print(f"Ola meu nome e {nome}!")
+idade = int(input("Digite sua idade: "))
+print(f"Voce tem {idade} anos!")
+peso = float(input("Digite seu peso: "))
+if peso < 1100:
+    print("Voce esta IMENSO BALEIA ENORME  do peso ideal.") 
